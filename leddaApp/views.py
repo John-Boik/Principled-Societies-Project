74acc3e0c9570babbf2f2acb8301d45e14b96db7
John-Boik <john.boik@itron.com>
2022-06-28 13:09:41 -0600

    ssl/tls, views.py

--- leddaApp/views.py
+++ leddaApp/views.py
@@ -33,8 +33,8 @@ app.config.update(dict(
     MAIL_PORT = MAIL_PORT, 
     MAIL_USERNAME = MAIL_USERNAME, 
     MAIL_PASSWORD = MAIL_PASSWORD,
-    #MAIL_USE_SSL = MAIL_USE_SSL,
-    MAIL_USE_TLS = MAIL_USE_TLS
+    MAIL_USE_TLS = True,
+    MAIL_USE_SSL = False
     ))
 
 mail = Mail(app)
